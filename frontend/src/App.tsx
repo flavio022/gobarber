41a85cc602546db601ed176c4e3a0e8ef6a357d8
Flavio <flavio.danilo022@gmail.com>
2020-05-18 08:34:26 -0300

    Iniciando o frontdend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import GlobalStyle from "./styles/global";
+import SignIn from "./Pages/SingIn";
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+export default App;
