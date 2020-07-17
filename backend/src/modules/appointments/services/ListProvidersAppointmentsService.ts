8810295b9a784e8424b61a8c6f5e60093eb58f12
Flavio <flavio.danilo022@gmail.com>
2020-07-17 19:31:25 -0300

    Implementation CacheProvider to RedisDB

--- backend/src/modules/appointments/services/ListProvidersAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProvidersAppointmentsService.ts
@@ -2,6 +2,8 @@ import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
+
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 interface IRequest {
@@ -14,7 +16,7 @@ interface IRequest {
 @injectable()
 class ListProviderAppointmentsService {
   constructor(
-    @inject('AppointmentsRepository')
+    @inject('AppointmentRepository')
     private appointmentsRepository: IAppointmentsRepository,
 
     @inject('CacheProvider')
@@ -28,7 +30,8 @@ class ListProviderAppointmentsService {
     year,
   }: IRequest): Promise<Appointment[]> {
     const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
-
+    const cacheData = await this.cacheProvider.recover(cacheKey);
+    console.log(cacheData);
     let appointments = await this.cacheProvider.recover<Appointment[]>(
       cacheKey,
     );
@@ -42,6 +45,7 @@ class ListProviderAppointmentsService {
           year,
         },
       );
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,9 +1,9 @@
 import { injectable, inject } from 'tsyringe';
-
 import AppError from '@shared/errors/AppErros';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/User';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -14,12 +14,21 @@ class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
-  public async execute({ user_id }: IRequest): Promise<User[]> {
-    const user = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+  public async execute({ user_id }: IRequest): Promise<User[] | null> {
+    let user = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
+
+    if (!user) {
+      const user = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+      await this.cacheProvider.save(`providers-list:${user_id}`, user);
+    }
 
     return user;
   }
