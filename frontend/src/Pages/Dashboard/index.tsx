f5fc73e13016ffb2423a3f4328cde02592e9d5f1
Flavio <flavio.danilo022@gmail.com>
2020-07-22 18:53:21 -0300

    Create Dashboard page

--- frontend/src/Pages/Dashboard/index.tsx
+++ frontend/src/Pages/Dashboard/index.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar
+} from "./styles";
+import { FiPower } from "react-icons/fi";
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import logoIMg from "../../assets/logo.svg";
+import { UserAuth } from "../../hooks/AuthContext";
+const Dashboard: React.FC = () => {
+  const { singOut, user } = UserAuth();
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoIMg} alt="GoBarber" />
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem-vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={singOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+      <Content>
+        <Schedule></Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
